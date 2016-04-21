4dcfcef777e3fd57ea1a315ac1af125ce2c5cc45
jsgoldb <jgoldb@yahoo.com>
2016-04-21 13:32:04 -0500

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -14,31 +14,39 @@ function randomNumber() {
   return Math.floor(Math.random());
 }
 
-function menu() {
-  $('#menu').on('click', function(e) {
+function handleMenuClick(e) {
     $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  }
+
+function menu() {
+  $('#menu').on('click', handleMenuClick);
 }
 
-function play() {
-  $('#play').on('click', function(e) {
+function handlePlayClick(e) {
     $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
     return "now playing " + songToPlay + " by " + theArtist;
-  });
+  }
+
+function play() {
+  $('#play').on('click', handlePlayClick);
 }
+
+function handlePauseClick(e) {
+    $('#songPaused').html(songToPlay + " is paused");
+  }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', handlePauseClick);
 }
 
-function show() {
-  $('#show').on('click', function(e) {
+function handleShowClick(e) {
     var songs = "";
     for (var key in jukeboxSongs) {
       songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
     }
     $('#showSongs').html(songs);
-  });
+  }
+
+function show() {
+  $('#show').on('click', handleShowClick);
 }
